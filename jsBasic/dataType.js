5282b56d111618fd09155c13e092df685e30c8c9
dkn1ght23 <mujammal.salman@gmail.com>
2021-06-05 02:29:47 +0600

    upto js data type

---
+++ jsBasic/dataType.js
@@ -0,0 +1,29 @@
+var number = 10; //var will over write previous data
+
+let number2 = 11; // let will throw error that is why we will use let
+
+const x = 20;  //we can not change constant
+
+//alert('This is an alert') //for pop up
+
+let user = 'Mujammal Ahmed';
+let age = '23';
+let message = 'Welcome to the course';
+
+console.log(user, age, message); //shows output
+
+console.log(typeof (age)); //to check datatype of the variable
+
+let n = 100000213616546546465n;
+console.log(typeof n);
+
+let name = null;
+console.log(typeof name); //null is a object
+
+let m = true;
+console.log(typeof m); //boolean
+
+let y = undefined;
+console.log(typeof y); //undefined
+
+console.log(typeof alert);//alert() is a function
